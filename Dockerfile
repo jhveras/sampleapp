9f1cadc5fc1492727caa592841211e5465f5bc21
Juan Veras <juan.veras@gmail.com>
2019-01-25 12:55:30 -0400

    Fixed port number

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,8 @@ COPY . /app
 # Install any needed packages specified in requirements.txt
 RUN pip install --trusted-host pypi.python.org -r requirements.txt
 
-# Make port 8080 available to the world outside this container
-EXPOSE 8080
+# Make port 4000 available to the world outside this container
+EXPOSE 4000
 
 # Define environment variable
 ENV NAME World
--- app.py
+++ app.py
@@ -18,8 +18,8 @@ def hello():
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}" \
-           "<h2>Hello</h2>"
+           "<h2>Sample</h2>"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=4000)
